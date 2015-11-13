11a8591920c88082c6660fdfc6109b6c7fb2518f
Trumaine Johnson <trumaine.johnson@aquilent.com>
2015-11-13 15:49:14 -0500

    Beta 2 - Release   *  Remove APKs from source control (will add to releases section of github)   * Optimize imports   * Increment versionCode to 2

--- app/src/main/java/gov/dol/childlabor/FullReportActivity.java
+++ app/src/main/java/gov/dol/childlabor/FullReportActivity.java
@@ -1,16 +1,10 @@
 package gov.dol.childlabor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 
 import com.joanzapata.pdfview.PDFView;
-
-import java.util.Arrays;
 
 public class FullReportActivity extends AppCompatActivity {
 
