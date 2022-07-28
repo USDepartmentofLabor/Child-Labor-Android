f85c981dbed1b820be3016c9ae1f7a9662156432
pbhatt17 <praveenbhatt2000@gmail.com>
2022-07-28 15:38:06 -0400

    modified country and goods xml file

--- app/src/main/java/gov/dol/childlabor/CountryXmlParser.java
+++ app/src/main/java/gov/dol/childlabor/CountryXmlParser.java
@@ -30,7 +30,7 @@ public class CountryXmlParser {
     public static CountryXmlParser fromContext(Context context) {
         InputStream stream = null;
         try {
-            stream = context.getAssets().open("countries_2020.xml");
+            stream = context.getAssets().open("countries_2021.xml");
         }
         catch(IOException e) {
             e.printStackTrace();
